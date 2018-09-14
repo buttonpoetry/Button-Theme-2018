7f4c61a440353d381d4306722e5f756f272b4e8c
Lionel Di Giacomo <lionel@buttonpoetry.com>
2018-09-14 16:38:45 -0700

    Further color and navigation customizations; Add wireframe HTML to front page template.

--- page-templates/front.php
+++ page-templates/front.php
@@ -4,95 +4,53 @@ Template Name: Front
 */
 get_header(); ?>
 
-<header class="front-hero" role="banner">
-	<div class="marketing">
-		<div class="tagline">
-			<h1><?php bloginfo( 'name' ); ?></h1>
-			<h4 class="subheader"><?php bloginfo( 'description' ); ?></h4>
-			<a role="button" class="download large button sites-button hide-for-small-only" href="https://github.com/olefredrik/foundationpress">Download FoundationPress</a>
-		</div>
-
-		<div class="watch">
-			<span id="stargazers"><a href="https://github.com/olefredrik/foundationpress">1.5k stargazers</a></span>
-			<span id="twitter"><a href="https://twitter.com/olefredrik">@olefredrik</a></span>
-		</div>
+<div class="main-container">
+	<div class="main-grid">
+		<main class="main-content">
+<section class="showcase">
+	<p class="lead">We showcase the power and diversity of voices in our community becase we believe that poetry is fo everyone.</p>
+	<div class="grid-x">
+		<div class="cell small-4 medium-2"><img src="https://via.placeholder.com/150x225?text=Poems"></div>
+		<div class="cell small-4 medium-2"><img src="https://via.placeholder.com/150x225?text=Poems"></div>
+		<div class="cell small-4 medium-2"><img src="https://via.placeholder.com/150x225?text=Poems"></div>
+		<div class="cell small-4 medium-2"><img src="https://via.placeholder.com/150x225?text=Poems"></div>
+		<div class="cell small-4 medium-2"><img src="https://via.placeholder.com/150x225?text=Poems"></div>
+		<div class="cell small-4 medium-2"><img src="https://via.placeholder.com/150x225?text=Poems"></div>
 	</div>
-
-</header>
-
-<?php do_action( 'foundationpress_before_content' ); ?>
-<?php while ( have_posts() ) : the_post(); ?>
-<section class="intro" role="main">
-	<div class="fp-intro">
-
-		<div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-			<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
-			<div class="entry-content">
-				<?php the_content(); ?>
-			</div>
-			<footer>
-				<?php
-					wp_link_pages(
-						array(
-							'before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ),
-							'after'  => '</p></nav>',
-						)
-					);
-				?>
-				<p><?php the_tags(); ?></p>
-			</footer>
-			<?php do_action( 'foundationpress_page_before_comments' ); ?>
-			<?php comments_template(); ?>
-			<?php do_action( 'foundationpress_page_after_comments' ); ?>
-		</div>
-
-	</div>
-
+	<button>Shop all books</button>
 </section>
-<?php endwhile; ?>
-<?php do_action( 'foundationpress_after_content' ); ?>
-
-<div class="section-divider">
-	<hr />
+<section class="featured">
+	
+</section>
+<section class="bulletin">
+	<h2>Latest News</h2>
+	<p>We love seeing our poets getting the recognition they deserve. Follow us on <a href="#">Twitter</a> for more news about what goes on behind the scenes.</p>
+	<blockquote>
+		Congratulations to Denice Fohman on topping 3,000,000 views on their poem, "Dear Straight People"<br/>
+		Best of Button: Diksha Bijlani & Neil Hilborn<br />
+		Best of Button: Kevin Kantor & Steven Willis<br />
+		Congratulations to Omar Holmon on topping 100,000 views on "Batman's superpower is white privilege."
+	</blockquote>
+	<div class="meet-our-poets"><a href="#">Meet our poets</a></div>
+</section>
+<section class="instagram">
+	<h2>Instagram</h2>
+	<div class="grid-x">
+		<div class="cell small-4 medium-2"><img src="https://via.placeholder.com/250x250?text=Instagram"></div>
+		<div class="cell small-4 medium-2"><img src="https://via.placeholder.com/250x250?text=Instagram"></div>
+		<div class="cell small-4 medium-2"><img src="https://via.placeholder.com/250x250?text=Instagram"></div>
+		<div class="cell small-4 medium-2"><img src="https://via.placeholder.com/250x250?text=Instagram"></div>
+		<div class="cell small-4 medium-2"><img src="https://via.placeholder.com/250x250?text=Instagram"></div>
+		<div class="cell small-4 medium-2"><img src="https://via.placeholder.com/250x250?text=Instagram"></div>
+		<div class="cell small-4 medium-2"><img src="https://via.placeholder.com/250x250?text=Instagram"></div>
+		<div class="cell small-4 medium-2"><img src="https://via.placeholder.com/250x250?text=Instagram"></div>
+		<div class="cell small-4 medium-2"><img src="https://via.placeholder.com/250x250?text=Instagram"></div>
+		<div class="cell small-4 medium-2"><img src="https://via.placeholder.com/250x250?text=Instagram"></div>
+		<div class="cell small-4 medium-2"><img src="https://via.placeholder.com/250x250?text=Instagram"></div>
+		<div class="cell small-4 medium-2"><img src="https://via.placeholder.com/250x250?text=Instagram"></div>
+	</div>
+</section>
+</main>
 </div>
-
-
-<section class="benefits">
-	<header>
-		<h2>Build Foundation based sites, powered by WordPress</h2>
-		<h4>Foundation is the professional choice for designers, developers and teams. <br /> WordPress is by far, <a href="http://trends.builtwith.com/cms">the world's most popular CMS</a> (currently powering 38% of the web).</h4>
-	</header>
-
-	<div class="semantic">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/demo/semantic.svg" alt="semantic">
-		<h3>Semantic</h3>
-		<p>Everything is semantic. You can have the cleanest markup without sacrificing the utility and speed of Foundation.</p>
-	</div>
-
-	<div class="responsive">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/demo/responsive.svg" alt="responsive">
-		<h3>Responsive</h3>
-		<p>You can build for small devices first. Then, as devices get larger and larger, layer in more complexity for a complete responsive design.</p>
-
-	</div>
-
-	<div class="customizable">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/demo/customizable.svg" alt="customizable">
-		<h3>Customizable</h3>
-		<p>You can customize your build to include or remove certain elements, as well as define the size of columns, colors, font size and more.</p>
-
-	</div>
-
-	<div class="professional">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/demo/professional.svg" alt="professional">
-		<h3>Professional</h3>
-		<p>Millions of designers and developers depend on Foundation. We have business support, training and consulting to help grow your product or service.</p>
-	</div>
-
-	<div class="why-foundation">
-		<a href="/kitchen-sink">See what's in Foundation out of the box →</a>
-	</div>
-
-</section>
-
+</div>
 <?php get_footer();
