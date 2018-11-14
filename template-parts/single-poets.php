66a5d76e20464e83efc233e2601ce6cfd6d0100b
Lionel Di Giacomo <lionel@buttonpoetry.com>
2018-11-13 17:00:13 -0800

    Part of prior commit.

--- template-parts/single-poets.php
+++ template-parts/single-poets.php
@@ -2,74 +2,87 @@
 /**
  * The template for displaying Poet pages.
  *
- * @package Button-Theme-2018
- * @since Button-Theme-2018 0.1.0
+ * @package Button-Theme
+ * @since Button-Theme 0.1.0
  */
  ?>
 
-<div class="main-container extra-wide">
+<?php while ( have_posts() ) : the_post(); ?>
+
+<div class="poet-bio-wrapper">
+	<div class="main-container wide">
+		<div class="main-grid">
+			<main class="main-content-full-width">
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<section class="grid-container">	
+						<div class="grid-x">
+							<div class="poet-bio-content cell entry-content">
+								<img class="poet-bio-pic" src="<?php the_post_thumbnail_url( 'full' ) ?>">
+								<header>
+									<?php
+										if ( is_single() ) {
+											the_title( '<h1 class="poet-name entry-title">', '</h1>' );
+										} else {
+											the_title( '<h2 class="poet-name entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+										}									
+									?>
+									<p class="poet-bio-social">Twitter <a href="#social1">@Neilicorn</a> | Facebook <a href="#social1">@neilhilborn</a></p>
+								</header>
+								<?php the_content(); ?>
+								<?php edit_post_link( __( '(Edit)', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
+							</div>
+						</div>
+					</section>
+				</article>
+			</main>
+		</div>
+	</div>
+</div>
+
+<div class="main-container">
 	<div class="main-grid">
-		<main class="main-content-full-width">
-			<?php while ( have_posts() ) : the_post(); ?>
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<section class="poet-bio grid-container">	
-					<div class="grid-x">
-						<div class="poet-bio-content cell entry-content">
-							<img class="poet-bio-pic" src="<?php the_post_thumbnail_url( 'full' ) ?>">
-							<header>
-								<?php
-									if ( is_single() ) {
-										the_title( '<h1 class="poet-name entry-title">', '</h1>' );
-									} else {
-										the_title( '<h2 class="poet-name entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-									}									
-								?>
-								<p class="poet-bio-social">Twitter <a href="#social1">@Neilicorn</a> | Facebook <a href="#social1">@neilhilborn</a></p>
-							</header>
-							<?php the_content(); ?>
-							<?php edit_post_link( __( '(Edit)', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
-						</div>
+		<div class="main-content-full-width">
+			<section class="poet-featured-item grid-container">	
+				<div class="grid-x">
+					<div class="poet-featured-item-copy cell small-12 medium-6">
+						<h2>The Future</h2>
+						<h3>Now available for order!</h3>
+						<p>Neil Hilborn's highly anticipated second collection of poems, The Future, invites readers to find comfort in hard nights and better days. Filled with nostalgia, love, heartbreak, and the author’s signature wry examinations of mental health, Neil Hilborn’s second book helps explain what lives inside us, what we struggle to define. Written on the road over two years of touring, The Future is rugged, genuine, and relatable. Grabbing attention like gravity, Hilborn reminds readers that no matter how far away we get, we eventually all drift back together. These poems are fireworks for the numb. In the author’s own words, The Future is a blue sky and a full tank of gas, and in it, we are alive.</p>
+						<a class="button" href="product-link">Buy Now</a>
 					</div>
-				</section>
-				<section class="poet-featured-item grid-container">	
-					<div class="grid-x">
-						<div class="poet-featured-item-copy cell small-12 medium-6">
-							<h2>The Future</h2>
-							<h3>Now available for order!</h3>
-							<p>Neil Hilborn's highly anticipated second collection of poems, The Future, invites readers to find comfort in hard nights and better days. Filled with nostalgia, love, heartbreak, and the author’s signature wry examinations of mental health, Neil Hilborn’s second book helps explain what lives inside us, what we struggle to define. Written on the road over two years of touring, The Future is rugged, genuine, and relatable. Grabbing attention like gravity, Hilborn reminds readers that no matter how far away we get, we eventually all drift back together. These poems are fireworks for the numb. In the author’s own words, The Future is a blue sky and a full tank of gas, and in it, we are alive.</p>
-							<a class="button" href="product-link">Buy Now</a>
-						</div>
-						<div class="poet-featured-item-pic cell small-12 medium-6">
-							<img src="https://via.placeholder.com/150x300/">
-						</div>
+					<div class="poet-featured-item-pic cell small-12 medium-6">
+						<img src="https://via.placeholder.com/150x300/">
 					</div>
-				</section>
-				<section class="poet-product-carousel grid-container">	
-					<div class="grid-x">						
-						<?php for($i = 0; $i < 5; $i++) { ?>
-						<div class="cell small-12 medium-4 large-3">
-							<img src="https://via.placeholder.com/80x120/">
+				</div>
+			</section>
+	
+			<section class="poet-product-carousel grid-container">	
+				<div class="grid-x">						
+					<?php for($i = 0; $i < 5; $i++) { ?>
+					<div class="cell small-12 medium-4 large-3">
+						<img src="https://via.placeholder.com/80x120/">
 							<h3>Product Title</h3>
 							<a href="product-link">BUY NOW</a>
 						</div>
 						<?php } ?>
 					</div>
-				</section>
-				<footer>
-					<?php
-						wp_link_pages(
-							array(
-								'before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ),
-								'after'  => '</p></nav>',
-							)
-						);
-					?>
-					<?php $tag = get_the_tags(); if ( $tag ) { ?><p><?php the_tags(); ?></p><?php } ?>
-				</footer>
-			</article>
-			<?php comments_template(); ?>
-			<?php endwhile; ?>
-		</main>
+			</section>
+			<footer>
+				<?php
+					wp_link_pages(
+						array(
+							'before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ),
+							'after'  => '</p></nav>',
+						)
+					);
+				?>
+				<?php $tag = get_the_tags(); if ( $tag ) { ?><p><?php the_tags(); ?></p><?php } ?>
+			</footer>			
+			<?php comments_template(); ?>	
+		</div>
 	</div>
 </div>
+
+<?php endwhile; ?>
+
 <?php
