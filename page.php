87cdeeb268d627bbc07607c04890e562484eeefd
Lionel Di Giacomo <lionel@buttonpoetry.com>
2018-12-13 10:47:54 -0800

    Remove commented code.

--- page.php
+++ page.php
@@ -17,8 +17,7 @@ get_header(); ?>
 	<div class="main-grid">
 		<main class="main-content">
 			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-				<!-- Don't display comments on pages. ?php comments_template(); ?-->
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>				
 			<?php endwhile; ?>
 		</main>
 		<?php get_sidebar(); ?>
