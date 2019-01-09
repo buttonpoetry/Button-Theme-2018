018b9911a83f312271c27f6c3be59b5975f5b653
Lionel Di Giacomo <lionel@buttonpoetry.com>
2019-01-08 18:31:01 -0800

    Add social icons to footer and theme assets.

--- footer.php
+++ footer.php
@@ -11,8 +11,17 @@
 
 <footer class="footer">
     <div class="footer-container">
+
+        <div class="footer-social-icon-tray">
+            <a class="footer-social-icon" href="https://www.instagram.com/buttonpoetry/?hl=en"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/theme/social/instagram.svg"></a>
+            <a class="footer-social-icon" href="http://www.youtube.com/buttonpoetry/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/theme/social/youtube.svg"></a>
+            <a class="footer-social-icon" href="http://buttonpoetry.tumblr.com/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/theme/social/tumblr.svg"></a>
+            <a class="footer-social-icon" href="http://www.facebook.com/buttonpoetry"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/theme/social/facebook.svg"></a>
+            <a class="footer-social-icon" href="https://twitter.com/buttonpoetry"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/assets/images/theme/social/twitter.svg"></a>
+        </div>
+
         <p>Copyright &copy; <?php echo date('Y'); ?> Button Poetry</p>
-        <p>This site uses cookies. Take a look at Button's <a href="#">Privacy Policy</a>.</p>
+        <p>This site uses cookies. Take a look at Button's <a href="<?php echo get_privacy_policy_url(); ?>">Privacy Policy</a>.</p>
         <!-- div class="footer-grid">
             <?php /*dynamic_sidebar( 'footer-widgets' );*/ ?>
         </div -->
