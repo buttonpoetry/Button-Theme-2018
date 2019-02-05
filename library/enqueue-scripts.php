e3197fc19177b54edc7d6b9cef683db6b086f1b8
Lionel Di Giacomo <lionel@buttonpoetry.com>
2019-02-04 16:36:54 -0800

    Some WooCommerce CSS & Anchor CSS consolidation.
    Preliminary adjustments to product cards.
    
    Setting WC styles as dependencies for theme styles for inheritance.

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -36,8 +36,11 @@ endif;
 if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	function foundationpress_scripts() {
 
+		// Set WooCommerce styles as dependencies for our stylesheet, so ours load later.
+		$css_dependencies = class_exists( 'WooCommerce' ) ? array( 'woocommerce-layout', 'woocommerce-smallscreen', 'woocommerce-general' ) : array();
+		
 		// Enqueue the main Stylesheet.
-		wp_enqueue_style( 'main-stylesheet', get_stylesheet_directory_uri() . '/dist/assets/css/' . foundationpress_asset_path( 'app.css' ), array(), '2.10.4', 'all' );
+		wp_enqueue_style( 'main-stylesheet', get_stylesheet_directory_uri() . '/dist/assets/css/' . foundationpress_asset_path( 'app.css' ), $css_dependencies, '2.10.4', 'all' );
 
 		// Deregister the jquery version bundled with WordPress.
 		wp_deregister_script( 'jquery' );
