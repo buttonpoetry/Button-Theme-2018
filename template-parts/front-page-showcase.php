d3502af95cae18629c72da265c46ab81c0abaa54
Lionel Di Giacomo <lionel@buttonpoetry.com>
2019-02-05 19:26:03 -0800

    Repair new short lead feature.

--- template-parts/front-page-showcase.php
+++ template-parts/front-page-showcase.php
@@ -9,7 +9,7 @@
 
 <section class="showcase">
 <p class="lead hide-for-large"><?php 
-    echo get_theme_mod('front_page_showcase_lead_short', __( 'Poetry is for everyone!', 'foundationpress' ));
+    echo get_theme_mod('front_page_showcase_lead_small', __( 'Poetry is for everyone!', 'foundationpress' ));
 ?></p>
 <p class="lead show-for-large"><?php 
     echo get_theme_mod('front_page_showcase_lead', __( 'We showcase the power and diversity of voices in our community because we believe that poetry is for everyone.', 'foundationpress' ));
