254b28c49e9a821989c79c52906f2862989a4ae8
Lionel Di Giacomo <lionel@buttonpoetry.com>
2019-02-15 17:07:35 -0800

    Prevent cart from displaying incorrectly after removing items.
    Uses PHP. Ajax version could be implemented for JS enabled visitors.

--- template-parts/bp-mini-cart.php
+++ template-parts/bp-mini-cart.php
@@ -15,8 +15,8 @@
         ?> id="bp-mini-cart" class="bp-mini-cart dropdown-pane" data-dropdown data-hover="true" data-hover-pane="true" data-position="bottom" data-alignment="right"> <?php 
     } else {
         ?> class="bp-mini-cart"> <?php
-    }
-    if ( ! WC()->cart->is_empty() ) {
+    }    
+    if ( ! WC()->cart->is_empty() && ! is_cart() ) {
         echo '<table><thead><tr><th>#</th><th>Item</th><th>$</th></thead><tbody>';
         foreach ( WC()->cart->get_cart() as $cart_item ) {
             if ( empty( $cart_item[ 'composite_parent' ] ) ) 
@@ -34,6 +34,9 @@
     ?><a class="button secondary" href="<?php echo wc_get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php echo sprintf ( _n( '%d item', '%d items', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?> - <?php echo WC()->cart->get_cart_total(); ?></a>
     <em class="mini-shipping-note">*Cart total before shipping</em>
     <?php }
+    else if ( is_cart() ) {
+        echo "<a href='" . site_url('/shop') . "/'><em>You're viewing your cart.<br>Click here to visit the shop!</em></a>";
+    }
     else {
         echo "<a href='" . site_url('/shop') . "/'><em>Your cart is empty,<br>check out the shop!</em></a>";
         } ?>        
