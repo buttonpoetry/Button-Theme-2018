dc826040cd8e5944fd430689ede5be02cbb9030b
Lionel Di Giacomo <lionel@buttonpoetry.com>
2019-02-22 14:24:41 -0800

    Tweak menu logo display spacing, padding, and hover effects.
    Made top-bar and title bar transparent by default.
    
    May need to consider background-color strategy for sicky header.

--- template-parts/mobile-off-canvas.php
+++ template-parts/mobile-off-canvas.php
@@ -9,7 +9,7 @@
 ?>
 
 <nav class="mobile-off-canvas-menu off-canvas position-right" id="<?php foundationpress_mobile_menu_id(); ?>" data-off-canvas data-auto-focus="false" role="navigation">
-	<div class="off-canvas-logo-container"><img class="off-canvas-logo" src="<?php echo get_stylesheet_directory_uri() ?>/dist/assets/images/theme/button-logo-transparent-white.png"></div>
+	<div class="off-canvas-logo-container"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img class="off-canvas-logo" src="<?php echo get_stylesheet_directory_uri() ?>/dist/assets/images/theme/button-logo-transparent-white.png"></a></div>
 	<?php foundationpress_mobile_nav(); ?>
 </nav>
 
