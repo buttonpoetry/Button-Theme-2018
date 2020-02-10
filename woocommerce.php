340caad48f8fa4d4d5d7d89691d81122d5d5cfb1
Lionel Di Giacomo <lionel@buttonpoetry.com>
2020-02-10 13:09:38 -0800

    Conditionally hide Promotional Shop Page Content

--- woocommerce.php
+++ woocommerce.php
@@ -8,87 +8,91 @@
  * @since FoundationPress 1.0.0
  */
 
-get_header(); 
+get_header();
 
-if(is_shop()) {	?>
-	<div class="main-container full-width">
-		<?php bp_shipping_banner() ?>
-		<div class="main-grid">
-			<div class="main-content-full-width">
-			<?php		
-			get_template_part('template-parts/shop-page-featuredcat');
-			do_action('bp_shop_after_featuredcat');
-			get_template_part('template-parts/shop-page-bookcats');
-			get_template_part('template-parts/shop-page-merchcat');				
-			?>		
+if(is_shop()) {
+	$paged = get_query_var( 'paged', 1 );
+	if (!isset($_GET['orderby']) && $paged < 2) {
+		?><div class="main-container full-width">
+			<?php bp_shipping_banner() ?>
+			<div class="main-grid">
+				<div class="main-content-full-width">
+				<?php
+				get_template_part('template-parts/shop-page-featuredcat');
+				do_action('bp_shop_after_featuredcat');
+				get_template_part('template-parts/shop-page-bookcats');
+				get_template_part('template-parts/shop-page-merchcat');
+				?>
+				</div>
 			</div>
-		</div>
-	</div>
-	<?php get_template_part('template-parts/shop-page-products'); ?>
-	
-	<?php
-	// Function to trigger the slick slider on the shop page.
-	if ( !function_exists('bp_shop_page_slider') ) {
+		</div><?php
+		get_template_part('template-parts/shop-page-products');
 
-		add_action( 'wp_footer', 'bp_shop_page_slider', 100 );
-		function bp_shop_page_slider() {
-			?><script type="text/javascript"> 
-				
-				$("#showcase-slider").slick( {
-					arrows: false,
-					dots: true,
-					slidesToShow: 6,
-					adaptiveHeight: true,
-					responsive: [{
-						breakpoint: 900,
-						settings: {
-							slidesToShow: 3,
-							slidesToScroll: 3,
-							infinite: true,
-						}},
-						{
-						breakpoint: 600,
-						settings: {
-							slidesToShow: 3,
-							slidesToScroll: 3,
-							infinite: true,
-						}
-					}]
-				});			
-			</script><?php
-		}	
+		// Function to trigger the slick slider on the shop page.
+		if ( !function_exists('bp_shop_page_slider') ) {
+			add_action( 'wp_footer', 'bp_shop_page_slider', 100 );
+			function bp_shop_page_slider() {
+				?><script type="text/javascript">
+
+					$("#showcase-slider").slick( {
+						arrows: false,
+						dots: true,
+						slidesToShow: 6,
+						adaptiveHeight: true,
+						responsive: [{
+							breakpoint: 900,
+							settings: {
+								slidesToShow: 3,
+								slidesToScroll: 3,
+								infinite: true,
+							}},
+							{
+							breakpoint: 600,
+							settings: {
+								slidesToShow: 3,
+								slidesToScroll: 3,
+								infinite: true,
+							}
+						}]
+					});
+				</script><?php
+			}
+		}
+	}
+	else {
+		get_template_part('template-parts/shop-page-products');
 	}
 }
 else {
-?> 
+?>
 <div class="main-container">
 	<div class="main-grid">
 		<main class="main-content">
 			<?php
 				//Append format attribute to loop product links on the Audiobooks and E-Books pages.
-				if ( ! function_exists('bp_get_format_URL_att') && ( is_product_category('E-Books') || is_product_category('Audiobooks') ) ) { 
+				if ( ! function_exists('bp_get_format_URL_att') && ( is_product_category('E-Books') || is_product_category('Audiobooks') ) ) {
 					function bp_get_format_URL_att() {
 						$format_URL_att = "?attribute_pa_format=";
 						if( is_product_category('E-Books') ) { return $format_URL_att .= "e-book"; }
-						else { return $format_URL_att .= "audiobook"; }								
+						else { return $format_URL_att .= "audiobook"; }
 					};
-									
+
 					add_filter('woocommerce_loop_product_link', function( $title ) {
 						return $title . bp_get_format_URL_att();
-					}); 
+					});
 
 					add_filter('woocommerce_loop_add_to_cart_link', function( $atc_html ) {
 						$insert_pos = strpos( $atc_html, '" ');
 						$new_atc_html = substr($atc_html, 0, $insert_pos) . bp_get_format_URL_att() . substr($atc_html, $insert_pos);
 						return $new_atc_html;
-					});						
+					});
 				}
-					
+
 				//Add breadcrumbs.
 				if ( function_exists('yoast_breadcrumb') ) {
 					yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
-				}				
-				woocommerce_content(); ?>				
+				}
+				woocommerce_content(); ?>
 		</main>
 	<?php get_sidebar(); ?>
 	</div>
